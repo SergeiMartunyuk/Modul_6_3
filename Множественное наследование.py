88314226cf3cf704ee6fbaf2eb420cb6c17da1dd
SergeiMartunyuk <alessaralfinit@gmail.com>
2024-11-17 15:05:54 +0500

    Update Множественное наследование.py

--- Множественное наследование.py
+++ Множественное наследование.py
@@ -2,50 +2,47 @@ class Animal:
     live = True
     sound = None
     _DEGREE_OF_DANGER = 0
-
     def __init__(self, speed):
         self.speed = speed
-        _cords = [0, 0, 0]
+        self._cords = [0, 0, 0]
 
     def move(self, dx, dy, dz):
-        self.dx = dx * self.speed
-        self.dy = dy * self.speed
-        self.dz = dz * self.speed
+        self._cords[0] = dx * self.speed
+        self._cords[1] = dy * self.speed
+        self._cords[2] = dz * self.speed
+        if dz < 0:
+            print("It's too deep, i can't dive :(")
 
     def get_cords(self):
-        print(f'X: {self.dx}, Y: {self.dy}, Z: {self.dz}')
+        print(f'X: {self._cords[0]}, Y: {self._cords[1]}, Z: {self._cords[2]}')
 
     def attack(self):
-        self._DEGREE_OF_DANGER += self._DEGREE_OF_DANGER
         if self._DEGREE_OF_DANGER < 5:
             print("Sorry, i'm peaceful :)")
         else:
             print("Be careful, i'm attacking you 0_0")
 
-class Bird(Animal):
-    import random
-    num = [0, 1, 2, 3, 4]
-    beak = True
-    num_ = random.choice(num)
+    def speak(self):
+        print(self.sound)
 
+class Bird(Animal):
+    beak = True
     def lay_eggs(self):
+        import random
+        num = [0, 1, 2, 3, 4]
+        self.num_ = random.choice(num)
         print(f"Here are(is) {self.num_} eggs for you")
 
 class AquaticAnimal(Animal):
-    _DEGREE_OF_DANGER = 3
-
+    Animal._DEGREE_OF_DANGER = 3
     def dive_in(self, dz):
-        self.dz += dz * self.speed // -2
+        self._cords[2] -= abs(dz) // 2 * self.speed
 
 class PoisonousAnimal(Animal):
-    _DEGREE_OF_DANGER = 8
+    Animal._DEGREE_OF_DANGER = 8
 
 class Duckbill(Bird, AquaticAnimal, PoisonousAnimal):
-
-    def speak(self):
-        self.sound = "Click-click-click"
-        print(self.sound)
-
+    Animal.sound = "Click-click-click"
 
 db = Duckbill(10)
 
